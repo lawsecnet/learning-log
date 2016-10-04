17c7807655d7a73548d0f962a04251f06ff8f81b
lawsecnet <kamilbojarski7@gmail.com>
2016-10-04 11:28:48 +0200

    base templates and initial html structure created

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
 def index(request):
     # home page for learning learning_log
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    # show all topics
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
